0f994babb3364768e488f3503a02f17f612e571d
烈星氷刃 <101722929+Contemelia@users.noreply.github.com>
2024-02-06 13:22:18 +0300

    Update OpExpert.py | ID: 2024020602
    Added a few other necessary methods

--- BroadBITS/OpExpert.py
+++ BroadBITS/OpExpert.py
@@ -20,6 +20,17 @@ class OpExpert:
     
     
     
+    def initialLogin(self):
+        data = {
+            'user_auth': {
+                'user_name': self.opexpertUsername, 
+                'password': self.opexpertPassword
+            }
+        }
+        self.opexpertSessionID = self.performRequest(['login', dumps(data)]).get('id')
+    
+    
+    
     def performRequest(self, arguments, custom = None):
         
         # custom: any in [None, 'integration_login', 'integration_main']
@@ -39,7 +50,7 @@ class OpExpert:
             
             try:
                 parameters = parse.urlencode(payload).encode('utf-8')
-                response = request.urlopen(self.opexpertCRMURL, parameters).read().strip()
+                response = request.urlopen(self.opexpertURL, parameters).read().strip()
                 return loads(response.decode('utf-8'))
             except:
                 return False
@@ -59,6 +70,7 @@ class OpExpert:
             # arguments[8]: Data contents table
             # arguments[9]: Alias key
             # arguments[10]: Session ID
+            # arguments[11]: Name of the integration
             
             payload = loads(arguments[1])
             if custom == 'integration_main':
@@ -85,6 +97,90 @@ class OpExpert:
                     response = get(arguments[0], headers = header)
                     if response.status_code == 200:
                         return response.json()
-            except:
+            except Exception as error:
                 return False
+    
+    
+    
+    def getIntegrationList(self, arguments = ['', '', '', 0, [], {}, 1, False]):
+        
+        # arguments[0]: Name of the module
+        # arguments[1]: Query ('' by default)
+        # arguments[2]: Sort order ('' by default)
+        # arguments[3]: Offset value (0 by default)
+        # arguments[4]: Fields to select ([] by default)
+        # arguments[5]: Link-name fields ({} by default)
+        # arguments[6]: Maximum number of results to fetch (1 by default)
+        # arguments[7]: Whether to include deleted record [True/False] (False by default)
+        
+        integrationsPayload = {
+            'session': self.opexpertSessionID, 
+            'module_name': arguments[0], 
+            'query': arguments[1], 
+            'order_by': arguments[2], 
+            'offset': arguments[3], 
+            'select_fields': arguments[4], 
+            'link_name_to_fields_array': [arguments[5]],
+            'max_results': arguments[6], 
+            'deleted': arguments[7]
+        }
+        self.integrationList = self.performRequest(['get_entry_list', dumps(integrationsPayload)])
+    
+    
+    
+    def loginIntegration(self, integration):
+        
+        # integration: List that can have upto 3 API tasks
                 
+        if len(integration) in [2, 3]:
+            
+            # integration[0]: API URL
+            # integration[1]: Parameters to pass as payload
+            # integration[2]: Header (can either be in base64 or JSON format)
+            # integration[3]: Response format
+            # integration[4]: SSL Verify [True/False]
+            # integration[5]: Main API [True/False]
+            # integration[6]: Login API [True/False]
+            # integration[7]: Custom API filter
+            # integration[8]: Data contents table
+            # integration[9]: Alias key
+            # integration[10]: Name of the integration
+            # integration[11]: Session ID
+            
+            try:
+                for aPITask in integration:
+                    if aPITask[6]:
+                        sessionID = self.performRequest(aPITask, 'integration_login')
+                    return sessionID if sessionID else None, integration[9]
+            except Exception as error:
+                pass
+        
+        else:
+            return None, integration[9]
+    
+    
+    
+    def getDataIntegration(self, integration, sessionID):
+        
+        # integration[0]: API URL
+        # integration[1]: Parameters to pass as payload
+        # integration[2]: Header (can either be in base64 or JSON format)
+        # integration[3]: Response format
+        # integration[4]: SSL Verify [True/False]
+        # integration[5]: Main API [True/False]
+        # integration[6]: Login API [True/False]
+        # integration[7]: Custom API filter
+        # integration[8]: Data contents table
+        # integration[9]: Alias key
+        # integration[10]: Name of the integration
+        
+        try:
+            for aPIList in integration:
+                aPIList.append(sessionID)
+                if aPIList[5]:
+                    data = self.performRequest(aPIList, 'integration_main')
+                    return data, aPIList[7], aPIList[8]
+        except Exception as error:
+            print(error)
+            # print("getDataFromIntegration(): No input was given.")
+        return None
